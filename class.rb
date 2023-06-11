cc94406242a0048395c9c5b2e45f12c35b035a29
beckzod <jumabaevbeckzod@gmail.com>
2023-06-11 21:38:24 +0500

    Create class

---
+++ class.rb
@@ -0,0 +1,15 @@
+# ** Create class and get object
+class Person
+    def initialize(name, age)
+      @name = name
+      @age = age
+    end
+  
+    def say_hello
+      puts "Hello, my name is #{@name} and I'm #{@age} years old."
+    end
+  end
+  
+  person = Person.new("John", 23)
+  person.say_hello
+  
