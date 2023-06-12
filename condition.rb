4daacbcfe97182b923e9ec61f51545b0e1571204
beckzod <jumabaevbeckzod@gmail.com>
2023-06-12 20:11:29 +0500

    use to condition

---
+++ condition.rb
@@ -0,0 +1,56 @@
+#If statement 
+# puts "Enter number:"
+# number=gets.chomp.to_i
+# number=4
+
+# if number%2==0
+#     puts "an even number=#{number}"
+# else 
+#     puts "an odd number=#{number}"
+
+# end
+
+#Unless statement 
+
+# unless number > 10
+#     puts "number is not greater than 10"
+# else
+#     puts "number is greater than 10"
+# end
+
+#Case statement
+
+# day=gets.chomp
+
+# case day
+# when "Monday"
+#     puts "Today is #{day} "
+# when "Tuesday"
+#     puts "Today is #{day} "
+# when "Wednesday"
+#     puts "Today is #{day} "
+# when "Thursday"
+#     puts "Today is #{day} "
+# when "Friday"
+#     puts "Today is #{day} "
+# when "Saturday"
+#     puts "Today is #{day} "
+# else 
+#     puts "Today is #{day} "
+# end
+
+#While and Until statement
+
+# count=gets.chomp.to_i
+
+# while count<=5
+#     puts "Count:#{count}"
+#     count+=1
+# end
+
+# until count>=5
+#     puts "Count:#{count}"
+#     count+=1
+# end
+
+
