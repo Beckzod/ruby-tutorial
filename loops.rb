53fa0acbbd83c30b2ceb1ccb13bee470b469cf84
beckzod <jumabaevbeckzod@gmail.com>
2023-06-12 20:11:47 +0500

    Use to loops

---
+++ loops.rb
@@ -0,0 +1,26 @@
+# for loops
+# count=gets.chomp.to_i
+
+# for i in 1..count
+#     puts "Count:#{i}"
+# end
+
+
+# *!Each loop only arrays
+# fruits=['apple','banana','orange']
+
+# fruits.each do |fruit|
+#     puts "Count:#{fruit}"
+# end
+
+
+# loop do 
+
+# count=0
+# loop do
+#     puts "Count:#{count}"
+#     count+=1
+#     break if count>=5
+# end
+
+
